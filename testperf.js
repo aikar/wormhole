949609ab70486d20464d64a35e4f1367048f7cca
Aikar <aikar@aikar.co>
2011-10-11 20:24:33 -0400

    Test branch to demonstrate performance degredation in node 0.5.x modified wormhole to use the 0.5.x fork() IPC for testing.

---
+++ testperf.js
@@ -0,0 +1,32 @@
+var util = require('util'),
+    fork = require('child_process').fork;
+    
+var SEND_PER_TICK = 10;
+if (!process.argv[2]) {
+  fork(__filename, ['--child']).on('message', count);
+  fork(__filename, ['--child']).on('message', count);
+  var i = 0
+  var x = 0;
+  
+  var start = new Date()
+  function count() {
+    if (x++ > 5000) {
+      i++;
+      x = 0;
+      var diff = (new Date() - start) / 5000;
+      util.print("\rReceived " + i +"k messages - Rate: " + (i / diff).toFixed(1) + "k messages/sec");
+    }
+  }
+} else {
+  var data = {call: 'foo', args: [1,"2",3.2,2222,true,"false",{x:'y'}]};
+  function senddata() {
+    process.send(['Benchmark', data]);
+  }
+  function repeat() {
+    for (var i = 0; i < SEND_PER_TICK; i++) {
+      senddata();
+    }
+    process.nextTick(repeat);
+  };
+  repeat();
+}
