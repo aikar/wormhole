044d37a7a62a2e7decae2dba0126e71310efeb68
Aikar <aikar@aikar.co>
2011-10-12 22:35:32 -0400

    updating benchmarks to work on .4.x too and just be cooler

--- examples/benchmark_recv.js
+++ examples/benchmark_recv.js
@@ -1,29 +1,41 @@
+
+
+
+
+var SENDING_AGENTS = 2;
+var SAMPLE_EVERY = 5000;
+
+
+
+
+
 var
     Wormhole = require('../lib/wormhole'),
     net = require('net'),
     util = require('util'),
-    fork = require('child_process').fork;
-    
+    spawn = require('child_process').spawn;
 
-var start;
-    var i = 0
-    var x = 0;
+var start, i = 0, x = 0,SAMPLE_MULT  = SAMPLE_EVERY/1000;
 net.createServer(function(conn)
 {
     if (!start) start = new Date()
-    Wormhole(conn, 'Benchmark', function(msg)
+    Wormhole(conn, 'Benchmark', function(e, msg)
     {
-        //console.log(msg);
-        if (x++ > 10000) {
+        if (x++ > SAMPLE_EVERY) {
             i++;
             x = 0;
-            var diff = (new Date() - start) / 10000;
-            util.print("\rReceived " + i*10 +"k messages - Rate: " + (i / diff).toFixed(1) + "k messages/sec");
+            var diff = (new Date() - start) / SAMPLE_EVERY;
+            util.print("\rReceived " + i*SAMPLE_MULT +"k messages - Rate: " + (i / diff).toFixed(1) + "k messages/sec");
         }
+        //console.log("SERVER: Got line: ", dump(msg));
     }, "_msg", "server");
 }).listen(9911, function() {
-    // spawn the senders   
-    fork(__dirname + '/others/senddata.js');
-    fork(__dirname + '/others/senddata.js');
-    //fork(__dirname + '/others/senddata.js');
+    // spawn the senders
+    for (var i = 0; i < SENDING_AGENTS; i++) {
+        spawn(process.execPath, [__dirname + '/others/senddata.js'], {customFds: [-1, 1, 2]});
+    }
 });
+process.on('SIGINT', function() {
+    util.print("\n");
+    process.reallyExit();
+})
--- examples/others/senddata.js
+++ examples/others/senddata.js
@@ -13,14 +13,10 @@ client.on('connect', function()
   var data = '{"_":["Benchmark",{"call":"foo","args":[1,"2",3.2,2222,true,"false",{"x":"y"}]}]}' +"\n";
   
   function senddata() {
-    for(var i = 0; i < SEND_PER_TICK; i++) {
-      //console.log(data);
-      client.write(data);
-    }
+      while(client.write(data)) {}
   }
   client.on('drain', function() {
-    //console.log(process.pid, 'drain')
-    senddata();
+    process.nextTick(senddata);
   });
   senddata();
   
